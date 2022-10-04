c15dd67a88562ca5cf43087992b2b82a2c5f47a6
Vadym Hurnik <vad.com.z@gmail.com>
2022-10-04 18:18:53 +0300

    rename

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -5,16 +5,22 @@ using namespace std;
 
 int main()
 {
-	float alpha = 0;
-	float Z1 = 0, Z2 = 0, Z3 = 0;
+	float A = 0, B = 0, C = 0;
 
-	cout << "Enter value of alpha: ";
-	cin >> alpha;
+	cout << "Enter point A: ";
+	cin >> A;
 
-	Z1 = 1 - 1 / 4 - pow(sin(2 * alpha), 2) + cos(2 * alpha);
-	Z2 = pow(cos(alpha), 2) + pow(cos(alpha), 4);
-	Z3 = 2 * Z1 + 1 / Z2;
+	cout << "Enter point B: ";
+	cin >> B;
 
-	cout << "Values:\nZ1: " << Z1 << endl << "Z2: " << Z2 << endl << "Z3: " << Z3;
+	cout << "Enter point C (must be between A and B): ";
+	cin >> C;
+
+	do
+	{
+		cout << "Enter valid value: ";
+		cin >> C;
+	} while (A>C && C>B);
+
 	return 0;
 }
