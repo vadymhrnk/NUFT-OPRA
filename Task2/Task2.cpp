e7d4acc8d43870142cc55e8c7e15c91fb9917ca6
Vadym Hurnik <vad.com.z@gmail.com>
2022-10-04 19:21:34 +0300

    Task 2 + task 3

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include<math.h>
 
 using namespace std;
 
 int main()
 {
 	float A = 0, B = 0, C = 0;
+	float sizeProduct;
 
 	cout << "Enter point A: ";
 	cin >> A;
@@ -13,14 +13,25 @@ int main()
 	cout << "Enter point B: ";
 	cin >> B;
 
-	cout << "Enter point C (must be between A and B): ";
+	cout << "Enter point C (between A and B): ";
 	cin >> C;
 
-	do
+	while (C > B && C > A || C < B && C < A)
 	{
-		cout << "Enter valid value: ";
+		cout << "Re enter point C: ";
 		cin >> C;
-	} while (A>C && C>B);
+	}
+
+	if (B > C && C > A)
+	{
+		sizeProduct = (B - C) * (C - A);
+	}
+	else if (A > C && C > B)
+	{
+		sizeProduct = (A - C) * (C - B);
+
+	}
+	cout << "Product: " << sizeProduct;
 
 	return 0;
 }
---
+++ Task3/Task3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "           ____" << endl;
+	cout << "         / |   |\\" << endl;
+	cout << "  _____/ @ |   | \\" << endl;
+	cout << " |> . .    |   |   \\" << endl;
+	cout << "  \\  .     |||||     \\________________________" << endl;
+	cout << "   |||||||\\                                    )" << endl;
+	cout << "            \\                                 |" << endl;
+	cout << "             \\                                |" << endl;
+	cout << "               \\                             /" << endl;
+	cout << "                |   ____________------\     |" << endl;
+	cout << "                |  | |                ||    /" << endl;
+	cout << "                |  | |                ||  |" << endl;
+	cout << "                |  | |                ||  |" << endl;
+	cout << "                |  | |                ||  |" << endl;
+	cout << "               (__/_/                ((__/" << endl;
+	return 0;
+}
