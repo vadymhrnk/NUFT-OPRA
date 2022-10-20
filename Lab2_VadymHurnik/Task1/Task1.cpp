d62accf629f08bc1130ac9fdedc4de0cefffc9d5
Vadym Hurnik <vad.com.z@gmail.com>
2022-10-20 10:31:53 +0300

    Laboratory 2 - task 1 block scheme

--- Lab2_VadymHurnik/Task1/Task1.cpp
+++ Lab2_VadymHurnik/Task1/Task1.cpp
@@ -11,8 +11,21 @@ int main()
 	cin >> x;
 	cout << "Enter Y: ";
 	cin >> y;
+	/*
+	if (y = x * y > 0)
+	{
+		a = pow((x + y), 2) - sqrt(x * y);
+	}
+	else if (x * y < 0)
+	{
+		a = pow((x + y), 2) + sqrt(x * y);
+	}
+	else {
+		a = pow((x + y), 2) + 1;
+	}
+	*/
+	a = x * y > 0 ? pow((x + y), 2) - sqrt(x * y) : x * y < 0 ? pow((x + y), 2) + sqrt(x * y) : pow((x + y), 2) + 1;
 
-	y = x * y > 0 ? a = pow((x + y), 2) - sqrt(x * y) : x * y < 0 ? a = pow((x + y), 2) + sqrt(x * y) : a = pow((x + y), 2) + 1;
 	cout << "Result: " << a << endl;
 
 	return 0;
