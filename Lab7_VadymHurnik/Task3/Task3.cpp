24d4d28665a31c628278c23d2b74d1b7f709549b
Vadym Hurnik <vad.com.z@gmail.com>
2022-12-04 19:10:18 +0200

    some work done

--- Lab7_VadymHurnik/Task3/Task3.cpp
+++ Lab7_VadymHurnik/Task3/Task3.cpp
@@ -5,6 +5,37 @@ using namespace std;
 
 int main()
 {
+	const int row = 7, column = 7;
+	int a4[row][column];
+	int avg[row][column];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			a4[i][j] = rand() % 100 - 50;
+			cout << a4[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			if (j >= i && i <= row - 1 && j <= column - 1 - i)
+			{
+				cout << a4[i][j] <<"\t";
+				avg[i][j] = a4[i][j];
+
+			}
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
