a49e65cab3af510fcb621badeaffbeef6c4f85b4
Vadym Hurnik <vad.com.z@gmail.com>
2023-05-24 17:52:37 +0300

    task 2 ready, probably sum fix needed

--- Lab15_VadymHurnik/Task1/Task1.cpp
+++ Lab15_VadymHurnik/Task1/Task1.cpp
@@ -1,135 +1,142 @@
 #include <iostream>
-#include <cstdio>
 #include <string>
-#include <cstdlib> // Include the <cstdlib> header for random number generation
-#include <ctime>   // Include the <ctime> header to seed the random number generator
+#include <stdio.h>
+#include <ctime>
+
 using namespace std;
 
 void createTextMatrixFile(const char* filename) {
-    int rows, cols;
-    cout << "Enter the number of rows: ";
-    cin >> rows;
-    cout << "Enter the number of columns: ";
-    cin >> cols;
+	int size;
+	cout << "Enter size of square matrix: ";
+	cin >> size;
 
-    FILE* file;
-    if (fopen_s(&file, filename, "w") == 0) {
-        fprintf(file, "%d %d\n", rows, cols);
-        srand(time(0)); // Seed the random number generator
+	FILE* file;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int value = rand() % 101 - 50; // Generate random number from -50 to 50
-                fprintf(file, "%d ", value);
-            }
-            fprintf(file, "\n");
-        }
-        fclose(file);
-        cout << "Text matrix file created successfully." << endl;
-    }
-    else {
-        cout << "Error creating text matrix file." << endl;
-    }
+	if (fopen_s(&file, filename, "w") == 0) {
+		fprintf(file, "%d\n", size);
+		srand(time(0));
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				int value = rand() % 101 - 50;
+				fprintf(file, "%d ", value);
+			}
+			fprintf(file, "\n");
+		}
+		fclose(file);
+		cout << "Text matrix file created successfully." << endl;
+	}
+	else {
+		cout << "Error creating text matrix file." << endl;
+	}
 }
 
 void createTextStringsFile(const char* filename) {
-    int numStrings;
-    cout << "Enter the number of strings: ";
-    cin >> numStrings;
+	int numStrings;
+	cout << "Enter the number of strings: ";
+	cin >> numStrings;
 
-    FILE* file;
-    if (fopen_s(&file, filename, "w") == 0) {
-        cin.ignore();
-        for (int i = 0; i < numStrings; i++) {
-            string str;
-            cout << "Enter string " << (i + 1) << ": ";
-            getline(cin, str);
-            fprintf(file, "%s\n", str.c_str());
-        }
-        fclose(file);
-        cout << "Text strings file created successfully." << endl;
-    }
-    else {
-        cout << "Error creating text strings file." << endl;
-    }
+	FILE* file;
+	if (fopen_s(&file, filename, "w") == 0) {
+		cin.ignore();
+		for (int i = 0; i < numStrings; i++) {
+			string str;
+			cout << "Enter string " << (i + 1) << ": ";
+			getline(cin, str);
+			fprintf(file, "%s\n", str.c_str());
+		}
+		fclose(file);
+		cout << "Text strings file created successfully." << endl;
+	}
+	else {
+		cout << "Error creating text strings file." << endl;
+	}
 }
 
 void createBinaryMatrixFile(const char* filename) {
-    int rows, cols;
-    cout << "Enter the number of rows: ";
-    cin >> rows;
-    cout << "Enter the number of columns: ";
-    cin >> cols;
+	int size;
+	cout << "Enter size of square matrix: ";
+	cin >> size;
 
-    FILE* file;
-    if (fopen_s(&file, filename, "wb") == 0) {
-        fwrite(&rows, sizeof(int), 1, file);
-        fwrite(&cols, sizeof(int), 1, file);
-        srand(time(0));
+	FILE* file;
+	if (fopen_s(&file, filename, "wb") == 0) {
+		fwrite(&size, sizeof(int), 1, file);
+		fwrite(&size, sizeof(int), 1, file);
+		srand(time(0));
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int value = rand() % 101 - 50;
-                fwrite(&value, sizeof(int), 1, file);
-            }
-        }
-        fclose(file);
-        cout << "Binary matrix file created successfully." << endl;
-    }
-    else {
-        cout << "Error creating binary matrix file." << endl;
-    }
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				int value = rand() % 101 - 50;
+				fwrite(&value, sizeof(int), 1, file);
+			}
+		}
+		fclose(file);
+		cout << "Binary matrix file created successfully." << endl;
+	}
+	else {
+		cout << "Error creating binary matrix file." << endl;
+	}
 }
 
 void createBinaryColumnMatrixFile(const char* filename) {
-    int rows, cols;
-    cout << "Enter the number of rows: ";
-    cin >> rows;
-    cout << "Enter the number of columns: ";
-    cin >> cols;
+	int size;
+	cout << "Enter size of square matrix: ";
+	cin >> size;
 
-    FILE* file;
-    if (fopen_s(&file, filename, "wb") == 0) {
-        fwrite(&rows, sizeof(int), 1, file);
-        fwrite(&cols, sizeof(int), 1, file);
-        srand(time(0));
+	FILE* file;
+	if (fopen_s(&file, filename, "wb") == 0) {
+		fwrite(&size, sizeof(int), 1, file);
+		fwrite(&size, sizeof(int), 1, file);
+		srand(time(0));
 
-        for (int j = 0; j < cols; j++) {
-            for (int i = 0; i < rows; i++) {
-                int value = rand() % 101 - 50;
-                fwrite(&value, sizeof(int), 1, file);
-            }
-        }
-        fclose(file);
-        cout << "Binary column matrix file created successfully." << endl;
-    }
-    else {
-        cout << "Error creating binary column matrix file." << endl;
-    }
+		for (int j = 0; j < size; j++) {
+			for (int i = 0; i < size; i++) {
+				int value = rand() % 101 - 50;
+				fwrite(&value, sizeof(int), 1, file);
+			}
+		}
+		fclose(file);
+		cout << "Binary column matrix file created successfully." << endl;
+	}
+	else {
+		cout << "Error creating binary column matrix file." << endl;
+	}
 }
 
 int main() {
-    const int MAX_FILENAME_LENGTH = 100;
-    char textMatrixFilename[MAX_FILENAME_LENGTH];
-    char textStringsFilename[MAX_FILENAME_LENGTH];
-    char binaryMatrixFilename[MAX_FILENAME_LENGTH];
-    char binaryColumnMatrixFilename[MAX_FILENAME_LENGTH];
-    cout << "Enter the name of the text matrix file: ";
-    cin.getline(textMatrixFilename, MAX_FILENAME_LENGTH);
+	const int MAX_FILENAME_LENGTH = 100;
+	char textMatrixFilename[MAX_FILENAME_LENGTH];
+	char textStringsFilename[MAX_FILENAME_LENGTH];
+	char binaryMatrixFilename[MAX_FILENAME_LENGTH];
+	char binaryColumnMatrixFilename[MAX_FILENAME_LENGTH];
+	cout << "Enter the name of the text matrix file: ";
+	cin.getline(textMatrixFilename, MAX_FILENAME_LENGTH);
 
-    cout << "Enter the name of the text strings file: ";
-    cin.getline(textStringsFilename, MAX_FILENAME_LENGTH);
+	cout << "Enter the name of the text strings file: ";
+	cin.getline(textStringsFilename, MAX_FILENAME_LENGTH);
 
-    cout << "Enter the name of the binary matrix file: ";
-    cin.getline(binaryMatrixFilename, MAX_FILENAME_LENGTH);
+	cout << "Enter the name of the binary matrix file: ";
+	cin.getline(binaryMatrixFilename, MAX_FILENAME_LENGTH);
 
-    cout << "Enter the name of the binary column matrix file: ";
-    cin.getline(binaryColumnMatrixFilename, MAX_FILENAME_LENGTH);
+	cout << "Enter the name of the binary column matrix file: ";
+	cin.getline(binaryColumnMatrixFilename, MAX_FILENAME_LENGTH);
+	system("pause");
+	system("cls");
 
-    createTextMatrixFile(textMatrixFilename);
-    createTextStringsFile(textStringsFilename);
-    createBinaryMatrixFile(binaryMatrixFilename);
-    createBinaryColumnMatrixFile(binaryColumnMatrixFilename);
+	cout << "Creating text matrix file." << endl;
+	createTextMatrixFile(textMatrixFilename);
+	system("pause");
+	system("cls");
+	cout << "Creating text string file." << endl;
+	createTextStringsFile(textStringsFilename);
+	system("pause");
+	system("cls");
+	cout << "Creating binary matrix file." << endl;
+	createBinaryMatrixFile(binaryMatrixFilename);
+	system("pause");
+	system("cls");
+	cout << "Creating binary column matrix file." << endl;
+	createBinaryColumnMatrixFile(binaryColumnMatrixFilename);
 
-    return 0;
+	return 0;
 }
---
+++ Lab15_VadymHurnik/Task2/Task2.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void printMatrix(int** arr, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int** readMatrixFromFile(const string& filename, int& size) {
+	ifstream file(filename);
+	int** matrix = nullptr;
+
+	if (file.is_open()) {
+		file >> size;
+
+		matrix = new int* [size];
+		for (int i = 0; i < size; i++) {
+			matrix[i] = new int[size];
+			for (int j = 0; j < size; j++) {
+				file >> matrix[i][j];
+			}
+		}
+
+		file.close();
+	}
+	else {
+		cout << "Error opening file: " << filename << endl;
+	}
+
+	return matrix;
+}
+
+void writeRowSumsToFile(const string& filename, int* rowSums, int size) {
+	ofstream file(filename);
+
+	if (file.is_open()) {
+		for (int i = 0; i < size; i++) {
+			file << rowSums[i] << endl;
+		}
+
+		file.close();
+		cout << "Row sums written to file: " << filename << endl;
+	}
+	else {
+		cout << "Error opening file: " << filename << endl;
+	}
+}
+
+void deleteMatrix(int** matrix, int size) {
+	for (int i = 0; i < size; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+}
+
+void calculateRowSums(int** matrix, int size, int* rowSums) {
+	for (int i = 0; i < size; i++) {
+		int sum = 0;
+		for (int j = 0; j < size; j++) {
+			sum += matrix[i][j];
+		}
+		rowSums[i] = sum;
+	}
+}
+
+int main() {
+	string inputFilename;
+	string outputFilename;
+	int size;
+
+	while (true) {
+		cout << "Enter the input file name (or 'stop' to exit): ";
+		cin >> inputFilename;
+
+		if (inputFilename == "stop") {
+			break;
+		}
+
+		int** matrix = readMatrixFromFile("..\\Task1\\" + inputFilename, size);
+		printMatrix(matrix, size);
+
+		if (matrix != nullptr) {
+			cout << "Enter the output file name: ";
+			cin >> outputFilename;
+
+			int* rowSums = new int[size];
+			calculateRowSums(matrix, size, rowSums);
+
+			cout << "Row sums: ";
+			for (int i = 1; i < size; i++) {
+				cout << rowSums[i] << " ";
+			}
+			cout << endl;
+
+			writeRowSumsToFile(outputFilename, rowSums, size);
+
+			delete[] rowSums;
+			deleteMatrix(matrix, size);
+		}
+	}
+
+	return 0;
+}
